7be26f97bed61d13416163222691b537d33e4201
Ilhomxoja <97319142+Ilhom0549@users.noreply.github.com>
2022-03-16 08:59:51 +0500

    Api key removed
    xD limit not found

--- app/src/main/java/uz/ilkhomkhuja/youtubeclone/repository/YoutubeRepository.kt
+++ app/src/main/java/uz/ilkhomkhuja/youtubeclone/repository/YoutubeRepository.kt
@@ -5,6 +5,6 @@ import uz.ilkhomkhuja.youtubeclone.retrofit.ApiService
 class YoutubeRepository(private var apiService: ApiService) {
 
     suspend fun getVideoByTag(q: String) =
-        apiService.getVideoByTag("snippet", q, "AIzaSyCSasUx_n_PEjEmm9G0xu0UZS9Cqd-WhIY")
+        apiService.getVideoByTag("snippet", q, "YOUR_KEY_HERE")
 
 }
